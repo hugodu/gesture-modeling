2a98a0be29e34c9866abd2348edb5139fed93a7d
d.sashikanth <d.sashikanth@280ac562-4954-11de-9ab4-498524d4d007>
2009-08-06 20:35:33 +0000

    bin/mgestr.jar is a gui for administration of gesture sets. mgestr.sh starts and logs both the gesture server and the gui
    git-svn-id: http://multitouch-gestr.googlecode.com/svn/trunk@33 280ac562-4954-11de-9ab4-498524d4d007

--- bin/mgestr.sh
+++ bin/mgestr.sh
@@ -2,4 +2,13 @@
 
 export LD_LIBRARY_PATH=$(pwd)/libs/
 echo $LD_LIBRARY_PATH
-./multitouch-gestr
+echo "\n--- Starting Session ---" >> multitouch-gestr.log
+date >> multitouch-gestr.log
+echo "\n--- ---" >> multitouch-gestr.log
+./multitouch-gestr >> multitouch-gestr.log & 
+
+
+echo "\n--- Starting Session ---" >> mgestr.jar.log
+date >> mgestr.jar.log
+echo "\n--- ---" >> mgestr.jar.log
+java -jar mgestr.jar >> mgestr.jar.log &
