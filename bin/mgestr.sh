19778af2f6ec5f2f06d47c16e36c06fcbc5f7dc6
d.sashikanth <d.sashikanth@280ac562-4954-11de-9ab4-498524d4d007>
2009-08-24 18:17:59 +0000

    Updated binaries for and README with a link to the svn for the GUI.
    git-svn-id: http://multitouch-gestr.googlecode.com/svn/trunk@53 280ac562-4954-11de-9ab4-498524d4d007

--- bin/mgestr.sh
+++ bin/mgestr.sh
@@ -6,9 +6,3 @@ echo "\n--- Starting Session ---" >> multitouch-gestr.log
 date >> multitouch-gestr.log
 echo "\n--- ---" >> multitouch-gestr.log
 ./multitouch-gestr >> multitouch-gestr.log & 
-
-
-echo "\n--- Starting Session ---" >> mgestr.jar.log
-date >> mgestr.jar.log
-echo "\n--- ---" >> mgestr.jar.log
-java -jar mgestr.jar >> mgestr.jar.log &
